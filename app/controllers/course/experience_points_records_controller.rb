6d9aa6cd6bce62f4def9713b4f8e94ed18d27df0
Brian Koh <kxmbrian@gmail.com>
2015-12-15 17:25:24 +0800

    Basic Create, Read for EXP Records

---
+++ app/controllers/course/experience_points_records_controller.rb
@@ -0,0 +1,25 @@
+class Course::ExperiencePointsRecordsController < Course::ComponentController
+  load_resource :course_user, through: :course, class: CourseUser.name
+  load_and_authorize_resource :experience_points_record, through: :course_user, class: Course::ExperiencePointsRecord.name
+
+  def index # :nodoc:
+  end
+
+  def new # :nodoc:
+  end
+
+  def create # :nodoc:
+    if @experience_points_record.save
+      redirect_to course_course_user_experience_points_records_path(current_course, current_course_user),
+                  success: t('.success')
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def experience_points_record_params # :nodoc:
+    @experience_points_record_params ||= params.require(:experience_points_record).permit(:reason, :points_awarded)
+  end
+end
